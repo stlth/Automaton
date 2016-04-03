a85c988eeda4b4fa4d082ca6096df6ba8b32b8be
Cory Calahan <cory@stealthfield.com>
2016-04-02 22:40:01 -0700

    Added Internationalization Support

--- Automaton.psm1
+++ Automaton.psm1
@@ -1,8 +1,22 @@
 ﻿####################################################################################################
 # Name: Automaton                                                                                  #
 # Author: Cory Calahan                                                                             #
-# Date: 2016-02-27                                                                                 #
+# Date: 2016-04-02                                                                                 #
 ####################################################################################################
+
+#region Internationalization (Localization)
+$moduleRoot = Split-Path -Path $MyInvocation.MyCommand.Path -Parent
+# https://technet.microsoft.com/en-us/library/hh847854.aspx
+# A locale must be supplied, setting default as 'en-US':
+$Culture = 'en-US'
+# If a user's specific locale is available, use that instead:
+if (Test-Path -Path (Join-Path -Path $moduleRoot -ChildPath $PSUICulture))
+{
+	$Culture = $PSUICulture
+}
+# Import the localized information:
+Import-LocalizedData -BindingVariable LocalizedData -FileName Automaton.LocalizedData.psd1 -BaseDirectory $moduleRoot -UICulture $Culture -Verbose
+#endregion
 
 # Get functions to load:
 $Public = @(Get-ChildItem -Path "$PSScriptRoot\Public\*.ps1" -ErrorAction 'SilentlyContinue')
@@ -17,7 +31,7 @@ foreach ($import in @($Public + $Private))
 	}
 	catch
 	{
-		Write-Error -Message "Failed to import function: $($import.FullName): $PSItem"
+		Write-Error -Message -Message ([string]::Format($LocalizedData.ErrorModuleFailedToImportFunction,$import.FullName,$PSItem))
 	}
 }
 # Export functions:
